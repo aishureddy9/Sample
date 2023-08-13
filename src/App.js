6984734f01dade647371f3ec1569e55915b958de
aishureddy9 <aishu.cs99@gmail.com>
2023-08-13 14:24:04 -0400

    Updated changes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MapComponent from "./MapComponent";
+import "./styles.css";
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Map Interface</h1>
+      <MapComponent />
+    </div>
+  );
+}
